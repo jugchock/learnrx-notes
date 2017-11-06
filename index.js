06bd37e4d50429623bee6a91b6c2dbfc3649e303
Chris Chock <chris.chock@polaris.com>
2017-11-06 17:55:48 -0600

    listen to keypresses

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 window.addEventListener('load', () => {
+    const textbox = document.querySelector('#textbox');
+
     const vals = [1, 2, 3].map(x => x + 1);
     console.log(vals);
+
+    textbox.addEventListener('keypress', e => console.log(e));
 });
