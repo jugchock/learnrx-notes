4fcca25f02bc406b024707c9d806c203fc7a5ebd
Chris Chock <chris.chock@polaris.com>
2017-11-06 18:43:12 -0600

    use Observable.create()

--- notes.js
+++ notes.js
@@ -7,24 +7,20 @@ window.addEventListener('load', () => {
     keypresses.forEach(x => console.log(x.keyCode));
 
     function getWikipediaSearchResults(term) {
-        return {
-            forEach: observer => {
-                const url = `http://en.wikipedia.org/w/api.php?action=opensearch&format=` +
-                    `json&search=${encodeURIComponent(term)}&callback=?`;
-                let cancelled = false;
+        return Observable.create(observer => {
+            const url = `http://en.wikipedia.org/w/api.php?action=opensearch&format=` +
+                `json&search=${encodeURIComponent(term)}&callback=?`;
+            let cancelled = false;
 
-                $.getJSON(url, data => {
-                    if (!cancelled) {
-                        observer.next(data[1]);
-                        observer.complete();
-                    }
-                });
+            $.getJSON(url, data => {
+                if (!cancelled) {
+                    observer.next(data[1]);
+                    observer.complete();
+                }
+            });
 
-                return {
-                    dispose: () => cancelled = true
-                };
-            }
-        };
+            return () => cancelled = true;
+        });
     }
 
     function searchWikipedia(term) {
