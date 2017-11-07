7f678e0cef910f66c77d2efe97d7b49a416203b4
Chris Chock <chris.chock@polaris.com>
2017-11-06 21:59:29 -0600

    subscribe to search and close buttons

--- notes.js
+++ notes.js
@@ -2,8 +2,13 @@ window.addEventListener('load', () => {
     const Observable = Rx.Observable;
     const textbox = document.querySelector('#textbox');
     const output = document.querySelector('#output');
+    const searchButton = document.querySelector('#searchButton');
+    const closeButton = document.querySelector('#closeButton');
+    const searchForm = document.querySelector('#searchForm');
 
     const keypresses = Observable.fromEvent(textbox, 'keyup');
+    const searchButtonClicks = Observable.fromEvent(searchButton, 'click');
+    const closeButtonClicks = Observable.fromEvent(closeButton, 'click');
 
     function getWikipediaSearchResults(term) {
         return Observable.create(observer => {
@@ -22,12 +27,22 @@ window.addEventListener('load', () => {
         });
     }
 
-    const searchResultSets = keypresses
-        .throttleTime(500)
-        .map(key => textbox.value)
-        .distinctUntilChanged()
-        .filter(search => search.length > 0)
-        .switchMap(search => getWikipediaSearchResults(search).retry(3));
+    searchButtonClicks.forEach(() => {
+        searchForm.style.display = 'block';
+    });
+
+    closeButtonClicks.forEach(() => {
+        searchForm.style.display = 'none';
+    });
+
+    const searchResultSets = searchButtonClicks.switchMap(() =>
+        keypresses
+            .debounceTime(500)
+            .map(key => textbox.value)
+            .distinctUntilChanged()
+            .filter(search => search.length > 0)
+            .switchMap(search => getWikipediaSearchResults(search).retry(3))
+    );
 
     searchResultSets
         .forEach(result => output.innerHTML = JSON.stringify(result))
